2ea31f104dfa6e5ab3a6bde53351b19a601f0357
Gasha <gashapratamamahardika@gmail.com>
2025-03-08 22:35:09 +0700

    Add api.php --fix route list

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GudangController;
 
-Route::apiResource('gudang', GudangController::class);
+Route::get('/gudang', [GudangController::class, 'index']);
+Route::post('/gudang', [GudangController::class, 'store']);
+Route::get('/gudang/{id}', [GudangController::class, 'show']);
+Route::put('/gudang/{id}', [GudangController::class, 'update']);
+Route::delete('/gudang/{id}', [GudangController::class, 'destroy']);
 
 
-
-
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
